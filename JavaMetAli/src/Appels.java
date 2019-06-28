f19e69ab3a68c64ac67b30c699a467e5d127559d
AliTozlu <a.tozlu@opencore.nl>
2019-06-28 11:42:18 +0200

    Apples has been updated !

--- JavaMetAli/src/Appels.java
+++ JavaMetAli/src/Appels.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class Appels {
     public static void main(String args[]) {
         Scanner bucky = new Scanner(System.in);
-
+        System.out.println(bucky.nextLine());
     }
 }
