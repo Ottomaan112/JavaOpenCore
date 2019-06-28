6c3d22ccba141f50735572c90d25a265c6773bfa
AliTozlu <a.tozlu@opencore.nl>
2019-06-28 11:33:01 +0200

    First Commit

---
+++ JavaMetAli/src/Movies.java
@@ -0,0 +1,13 @@
+public class Movies {
+    public static void main(String args[]) {
+        int movies;
+        movies = 3;
+
+        System.out.println("I want ");
+        System.out.print(movies);
+        System.out.print(" movies");
+
+    }
+}
+
+
